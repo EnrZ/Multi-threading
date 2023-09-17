74b52db26ff18e9aa5d119fc1f0aec9187991aab
EnrZ <ez@ENPC>
2023-09-17 00:51:23 -0500

    Multithreading Hello World

--- src/main/java/dev/ez/MTproj/MTprojApplication.java
+++ src/main/java/dev/ez/MTproj/MTprojApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 //first exclude because I was getting an error after changing java 20 to 18, second to disable automatic login page
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
 
@@ -13,6 +17,16 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class MTprojApplication {
 
 	public static void main(String[] args) {
+
+		Executor executor = Executors.newSingleThreadExecutor();
+		executor.execute(() -> {try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		} System.out.println("Hello World");});
+
+
 		SpringApplication.run(MTprojApplication.class, args);
 	}
 
